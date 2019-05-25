20bd593adb2665faeb5a36e654ec2a94f66dbff3
krisparis <christophe.elavumoottil@gmail.com>
2019-05-25 18:32:38 +0200

    Add iterative solution

--- Factorial/solutions.java
+++ Factorial/solutions.java
@@ -10,12 +10,25 @@ public class Factorial
     } else {
       return n*factorialByRecursion(n-1);
     }      
-  }  
-  
+  }
+    
+  // If number n is large, it is better to use an iterative solution
+  // because less costly.
+  private static int factorialByIteration(int n)
+  {
+    
+    int fact = 1;
+    for (int i = 2; i <= n; i++ ) {
+      fact *= i;
+    }
+    
+    return fact;
+  }
+    
   public static void main(String[] args)
   {
     int num = 5;
-    System.out.print("Factorial of "+ num + " is " + factorialByRecursion(5));
+    System.out.print("Factorial of "+ num + " is " + factorialByIteration(5));
   }
 
 }
